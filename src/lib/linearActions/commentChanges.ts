a2566f7d0ddb29e6fba62c5730d1ab1ba58fc30e
Jamie Dubs <jamie@jamiedubs.com>
2023-09-04 11:34:55 -0400

    feat: reformat display of comments

--- src/lib/linearActions/commentChanges.ts
+++ src/lib/linearActions/commentChanges.ts
@@ -4,5 +4,5 @@ export const createCommentParser = (data: ILinearData<ICommentData>) => {
   const { data: comment, url } = data;
   const { issue, user } = comment;
   const { title } = issue;
-  return `**${user.name}** commented on (*${comment.body}*) on [${title}](${url})`;
+  return `**${user.name}** commented on [${title}](${url}):\n> *${comment.body}`;
 };
