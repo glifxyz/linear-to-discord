ca3043f7e5f531094f2aaaf3ddc4a6f40299451d
Jamie Dubs <jamie@jamiedubs.com>
2023-09-05 08:04:29 -0400

    feat: format comments as blockquotes
    Respects newlines, so they show as new paragraphs, but with
    blockquote formatting in-between for niceness

--- src/lib/linearActions/commentChanges.ts
+++ src/lib/linearActions/commentChanges.ts
@@ -1,8 +1,14 @@
 import type { ICommentData, ILinearData } from '@/@types';
 
+function formatAsBlockquote(text: string): string {
+  const lines = text.split('\n');
+  const prefixedLines = lines.map(line => '> ' + line);
+  return prefixedLines.join('\n');
+}
+
 export const createCommentParser = (data: ILinearData<ICommentData>) => {
   const { data: comment, url } = data;
   const { issue, user } = comment;
   const { title } = issue;
-  return `**${user.name}** commented on [${title}](${url}):\n> *${comment.body}`;
+  return `**${user.name}** commented on [${title}](${url}):\n${formatAsBlockquote(comment.body)}`;
 };
